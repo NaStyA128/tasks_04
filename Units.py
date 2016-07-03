7ac304690a8280d03709febee0dd0e78a606e752
NaStyA128 <n.a.s.t.y.a28v@gmail.com>
2016-07-04 02:12:26 +0300

    New structure

--- Units.py
+++ Units.py
@@ -1,96 +1,145 @@
 import random
-import math
+import time
+from abc import ABCMeta, abstractmethod, abstractproperty
 
 
-START_RECHARGE = 100
-END_RECHARGE = 2000
+class Unit:
+    __metaclass__ = ABCMeta
+    __health = None
+    __recharge = None
+    __next_attack_time = 0
+    prev_time = None
 
-
-class Unit(object):
-    health = None
-    recharge = None
-
-    def __init__(self):
-        self.health = 100
-        self.recharge = random.randint(START_RECHARGE, END_RECHARGE)
-
+    @abstractproperty
     def do_attack(self):
+        # атака
         pass
 
-    # def take_damage(self):
-        # pass
+    @abstractmethod
+    def take_damage(self, *args):
+        # получение атаки и блок
+        pass
 
     @property
     def get_recharge(self):
-        return self.recharge
+        return self.__recharge
+
+    def set_recharge(self, recharge):
+        self.__recharge = recharge
 
     @property
     def get_health(self):
-        return self.health
+        return self.__health
+
+    def set_health(self, health):
+        self.__health = health
+
+    @abstractproperty
+    def get_experience(self):
+        pass
+
+    @property
+    def get_next_attack_time(self):
+        return self.__next_attack_time
+
+    def set_next_attack_time(self, next_attack_time):
+        self.__next_attack_time = next_attack_time
+
+    def check_attack(self):
+        now = time.time() * 1000
+        if self.prev_time is None:
+            return True
+        else:
+            next_time = self.prev_time + self.get_recharge
+            if now >= next_time:
+                return True
+            else:
+                return False
 
 
-class Soldiers(Unit):
-
-    experience = 0
+class Solder(Unit):
+    __experience = 0
 
     def __init__(self):
-        super().__init__()
+        self.set_health(100)
+        self.set_recharge(random.randint(100, 2000) / 10000)
+
+    @property
+    def get_experience(self):
+        return self.__experience
+
+    def set_experience(self):
+        if self.__experience < 50:
+            self.__experience += 1
 
     @property
     def do_attack(self):
-        a = 0.5 * (1 + self.get_health/100) * \
-            random.randint(50+self.experience, 100)/100
-        return a
+        if self.get_health > 0 and self.check_attack():
+            soldiers_attack = 0.5 * (1 + self.get_health) * \
+                random.randint(50 + self.__experience, 100) / 100
+            self.set_experience()
+            self.prev_time = time.time() * 1000
+            return soldiers_attack
+        else:
+            return 0
 
-    # @property
-    # def take_damage(self, v):
-        # self.health = self.get_health - v
-        # a = 0.05 + self.experience/100
-        # return a
+    def take_damage(self, damage):
+        attack = damage - (0.05 + self.__experience / 1000)
+        self.set_health(self.get_health - attack)
 
 
-class Vechiles(Unit):
-
+class Vehicles(Unit):
     operators = []
 
     def __init__(self):
-        super().__init__()
-        op = random.randint(1, 3)
-        i = 0
-        self.operators = [Soldiers for _ in range(1, op)]
-        sum_health = 0
-        for j in self.operators:
-            sum_health += j.get_health
+        self.set_recharge(random.randint(1000, 2000) / 10000)
+        operator_count = random.randint(1, 3)
+        self.operators = [Solder() for _ in range(0, operator_count)]
+        list_operators = [i.get_health for i in self.operators]
+        self.set_health(sum(list_operators) / len(list_operators))
 
-        self.health = int(sum_health/len(self.operators))
-        self.recharge = random.randint(1000, END_RECHARGE)
+    def get_operators(self):
+        return self.operators
 
+    @property
+    def get_experience(self):
+        return sum([i.get_experience for i in self.operators])
+
+    @staticmethod
+    def alive(units):
+        res = False
+        for i in units:
+            if i.get_health > 0:
+                res = True
+                break
+        return res
+
+    @property
     def do_attack(self):
-        sr_geo = 1
-        for i in self.operators:
-            sr_geo *= i.do_attack
-        sr_geo = math.pow(sr_geo, (1/len(self.operators)))
-        a = 0.5 * (1 + self.health/100) * sr_geo
-        print(a)
-        return a
+        if self.get_health > 0 and self.check_attack() \
+                and self.alive(self.operators):
+            list_attack_soldiers = [i.do_attack for i in
+                                    self.operators]
+            vehicles_attack = 0.5 * (1 + self.get_health / 100) * (
+                sum(list_attack_soldiers) / len(list_attack_soldiers))
+            self.prev_time = time.time() * 1000
+            return vehicles_attack
+        else:
+            return 0
 
-    # def take_damage(self):
-        # a = 0.1 + sum(self.operators/100)
-        # return a
+    def take_damage(self, damage):
+        list_operators_experience = [i.get_experience / 1000 for i in
+                                     self.operators]
+        damage -= 0.1 + sum(list_operators_experience)
+        # 60% урона на машину
+        self.set_health(self.get_health - damage * 0.6)
+        # случайный оператор, который получит 20% урона
+        random_operator = random.randint(0, len(self.operators) - 1)
+        j = 0
+        while j < len(self.operators):
+            if j == random_operator:
+                self.operators[j].take_damage(damage * 0.2)
+            else:
+                self.operators[j].take_damage(damage * 0.1)
+            j += 1
 
-
-class Squard(object):
-
-    units = None
-
-    def __init__(self, solders, vechiles):
-        self.units = [Soldiers() for _ in range(1, solders)] + [Vechiles() for _ in range(1, vechiles)]
-
-    def get_power(self):
-        sum_at = 0
-        for i in self.units:
-            sum_at += i.do_attack
-        print(sum_at)
-
-# s = Squard(2, 2)
-# s.get_power()
